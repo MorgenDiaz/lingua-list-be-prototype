f9a73f7e7a0a8f44523ac7aa35e57886b1c307f3
Morgen Diaz <morgen@webslinger.works>
2023-04-02 17:50:57 -1000

    Route decorators can only be applied to functions that satisfy the express RequestHandler interface.

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,6 +1,11 @@
+import { RequestHandler } from "express";
 import "reflect-metadata";
 import { HttpMethods } from "./HttpMethods";
 import { MetadataKeys } from "./MetadataKeys";
+
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler;
+}
 
 function routeBinder(method: HttpMethods) {
   return function (path: string) {
