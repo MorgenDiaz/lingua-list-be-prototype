e102a4b4dcf04fe6902d229a5b2423f71fd59442
Morgen Diaz <morgen@webslinger.works>
2023-04-04 19:03:11 -1000

    Installed and configured cors to allow cross origin requests from all origins for development purposes.

--- build/index.js
+++ build/index.js
@@ -5,12 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
 const process_1 = require("process");
+const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
 const AppRouter_1 = require("./controllers/AppRouter");
 require("./controllers/ChallengeController");
 require("./controllers/VocabularyWordController");
 const PORT = process_1.env["PORT"] ? Number(process_1.env["PORT"]) : 3000;
 const app = (0, express_1.default)();
+app.use(cors_1.default);
 const appRouter = AppRouter_1.AppRouter.getInstance();
 app.use(appRouter);
 app.listen(PORT, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 require("dotenv").config();
 import { env } from "process";
+import cors from "cors";
 import express from "express";
 import { AppRouter } from "./controllers/AppRouter";
 import "./controllers/ChallengeController";
@@ -8,8 +9,9 @@ import "./controllers/VocabularyWordController";
 const PORT: Number = env["PORT"] ? Number(env["PORT"]) : 3000;
 
 const app = express();
-const appRouter = AppRouter.getInstance();
 
+app.use(cors);
+const appRouter = AppRouter.getInstance();
 app.use(appRouter);
 
 app.listen(PORT, () => {
